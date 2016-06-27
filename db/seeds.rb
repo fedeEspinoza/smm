27e63f2e7b37c3b8f1ff77d7a4a0874ed011d714
martinnicolas <ttincho77@gmail.com>
2016-06-27 18:45:45 -0300

    Modifico scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Prioridades
+# ===========
+
+Prioridad.create(descripcion: 'Alta')
+Prioridad.create(descripcion: 'Normal')
+Prioridad.create(descripcion: 'Baja')
+
+# Tipos de medidores
+# ===================
