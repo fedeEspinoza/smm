144afa13a74ac75bf54bfc0970b295db690bcb9e
martinnicolas <ttincho77@gmail.com>
2016-09-07 18:18:05 -0300

    Modifico form de UsuarioMedidor y mas notificaciones

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,4 +2,8 @@ class Usuario < ActiveRecord::Base
   belongs_to :categorium
   belongs_to :estado
   belongs_to :persona
+
+  def to_s
+  	"#{self.razon_social} - #{usuario.persona.to_s}"
+  end
 end
