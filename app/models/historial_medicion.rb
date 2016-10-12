2bdea5e7bd8605d47a8e3c3e593893b60d34ea43
martinnicolas <ttincho77@gmail.com>
2016-10-12 19:11:46 -0300

    Modifico notificacion de errores para en el resto de los forms

--- app/models/historial_medicion.rb
+++ app/models/historial_medicion.rb
@@ -2,4 +2,7 @@ class HistorialMedicion < ActiveRecord::Base
   belongs_to :novedad
   belongs_to :medidor
   belongs_to :user
+
+  validates :estado_medidor, :presence => { :message => "Debe completar el campo Estado medidor" }
+  validates :fecha_medicion, :presence => { :message => "Debe completar el campo Fecha de medición" }
 end
