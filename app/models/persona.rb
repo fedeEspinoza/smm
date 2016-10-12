9edd10bd3cb225e07ab4cf400203aa67b155307f
martinnicolas <ttincho77@gmail.com>
2016-10-12 19:33:43 -0300

    Descomento regex de validacion de apellidos y nombres

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -6,9 +6,9 @@ class Persona < ActiveRecord::Base
   validates :nro_documento, numericality: { only_integer: true, :message => "El campo Número de documento debe ser un valor entero"}
   validates :nro_documento, length: {minimum: 7, maximum: 8, :message => "El campo Número de documento debe tener 7 u 8 dígitos"}
   validates :apellido, :presence => { :message => "Debe completar el campo Apellido" }
-  #validates :apellido, :format => {:with => /\A[a-zA-Z]+\z/, :message => "El campo Apellido sólo debe contener letras" }
+  validates :apellido, :format => {:with => /\A[a-z ñáéíóúA-ZÑÁÉÍÓÚ]+\z/, :message => "El campo Apellido sólo debe contener letras" }
   validates :nombre, :presence => { :message => "Debe completar el campo Nombre" }
-  #validates :nombre, :format => {:with => /\A[a-zA-Z]+\z/, :message => "El campo Nombre sólo debe contener letras" }
+  validates :nombre, :format => {:with => /\A[a-z ñáéíóúA-ZÑÁÉÍÓÚ]+\z/, :message => "El campo Nombre sólo debe contener letras" }
   validates :telefono, :presence => { :message => "Debe completar el campo Teléfono" }
   validates :telefono, numericality: { only_integer: true, :message => "El campo Teléfono debe ser un valor entero"}
   validates :email, :presence => { :message => "Debe completar el campo Email" }
